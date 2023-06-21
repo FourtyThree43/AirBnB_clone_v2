0d481c047b66c66bc33d6c4bac1cf4f38fc5dec8
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-06-22 00:03:58 +0300

    Update remote

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -2,7 +2,13 @@
 """Defines ``DBStorage`` class """
 
 from models.base_model import Base
-from os import getenv
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+import os
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -15,20 +21,33 @@ class DBStorage:
 
     def __init__(self):
         """Constructor method """
-        user = getenv('HBNB_MYSQL_USER')
-        passwad = getenv('HBNB_MYSQL_PWD')
-        host_name = getenv('HBNB_MYSQL_HOST')
-        db = getenv('HBNB_MYSQL_DB')
-        env = getenv('HBNB_ENV')
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                                      .format(user, passwad, host_name, db))
-        if env == "test":
-            Base.metadata.drop_all(self.__engine)
+        self.__engine = create_engine(f"mysql+mysqldb://"
+                                      + f"{os.getenv('HBNB_MYSQL_USER')}:"
+                                      + f"{os.getenv('HBNB_MYSQL_PWD')}"
+                                      + f"@{os.getenv('HBNB_MYSQL_HOST')}/"
+                                      + f"{os.getenv('HBNB_MYSQL_DB')}",
+                                      pool_pre_ping=True)
+
+        if os.getenv("HBNB_MYSQL_USER") == "test":
+            # drop all tables
+            pass
 
     def all(self, cls=None):
         """Query objects on the current database session """
         Base.metadata.create_all(self.__engine)
-        Session = sessionmaker()
-        self.__session = Session(self.__engine)
+        Session = sessionmaker(bind=self.__engine)
+        self.__session = Session()
+
+        class_list = [User, State, City, Amenity, Place, Review]
+        class_dict = {}
         if cls:
-            query_rows = self.__session.query(cls)
+            for instance in self.__session.query(cls).all():
+                key = instance.__class__.__name__ + "." + instance.id
+                class_dict[key] = instance
+        else:
+            for obj in class_list:
+                for instance in self.__session.query(obj).all():
+                    key = instance.__class__.__name__ + "." + instance.id
+                    class_dict[key] = instance
+
+        return class_dict
