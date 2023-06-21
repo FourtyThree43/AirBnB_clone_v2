fd2e1a403d280f83e0ff548076ea48f5a28d59a1
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-06-21 20:17:07 +0300

    Uodate remote

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -7,6 +7,28 @@ import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("mysql+mysqldb://hbnb_dev:hbnb_dev_pwd@localhost"
-                       + "/hbnb_dev_db", pool_pre_ping=True)
+class DBStorage:
+    """Storage class that connects the application to a database """
+    __engine = None
+    __session = None
 
+    def __init__(self):
+        """Constructor method """
+        self.__engine = create_engine(f"mysql+mysqldb://"
+                                      + f"{os.getenv('HBNB_MYSQL_USER')}:"
+                                      + f"{os.getenv('HBNB_MYSQL_PWD')}"
+                                      + f"@{os.getenv('HBNB_MYSQL_HOST')}/"
+                                      + f"{os.getenv('HBNB_MYSQL_DB')}"
+                                      , pool_pre_ping=True)
+
+        if os.getenv("HBNB_MYSQL_USER") == "test":
+            # drop all tables
+            pass
+
+    def all(self, cls=None):
+        """Query objects on the current database session """
+        Base.metadata.create_all(self.__engine)
+        Session = sessionmaker()
+        self.__session = Session(self.__engine)
+        if cls:
+            query_rows = self.__session.query(cls)
