a55ff324ae348000351c1ccebd10d948ef2872a0
FourtyThree43 <shaqmwa@outlook.com>
2023-07-06 14:04:50 +0300

    Fixed create_html() {echo -e} added check_owner(){} - Timestamp: 1688641490

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,15 +4,14 @@
 # Colors for formatting output
 red='\e[0;31m'
 brown='\e[0;33m'
-# green='\e[1;32m'
-# blue='\e[1;34m'
+green='\e[1;32m'
+blue='\e[1;34m'
 reset='\033[0m'
 
 # Function to install packages
 function install() {
     if command -v "$1" &> /dev/null ; then
-        return
-        # echo -e "    ${green}Package already installed: ${brown}${1}${reset}"
+        echo -e "    ${green}Package already installed: ${brown}${1}${reset}"
     else
         echo -e "    Installing: ${brown}$1${reset}"
         sudo apt-get update -y -qq && sudo apt-get install -y "$1" -qq
@@ -27,8 +26,7 @@ function create_directory() {
     if [ ! -d "$directory" ]; then
         mkdir -p "$directory"
     else
-        return
-        # echo -e "    ${green}Directory already exists: ${brown}$directory${reset}"
+        echo -e "    ${green}Directory already exists: ${brown}$directory${reset}"
     fi
 }
 
@@ -36,7 +34,22 @@ function create_directory() {
 function create_html_file() {
     local file_path="/data/web_static/releases/test/index.html"
     local content="<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>"
-    echo "$content" > "$file_path"
+    echo -e "$content" > "$file_path"
+}
+
+# Function to Check/Update /data/ ownership.
+function check_owner() {
+    local owner=$(stat -c "%U" "/data/")
+    local group=$(stat -c "%G" "/data/")
+    local expected_owner="ubuntu"
+    local expected_group="ubuntu"
+
+    if [ "$owner" != "$expected_owner" ] || [ "$group" != "$expected_group" ]; then
+        echo -e "    ${brown}Changing ownership of /data/ to ubuntu:ubuntu...${reset}"
+        sudo chown -R ubuntu:ubuntu /data/ || handle_error 1 "Failed to change ownership of /data/"
+    else
+        echo -e "    ${green}Ownership of /data/ is already set: ${brown}ubuntu:ubuntu.${reset}"
+    fi
 }
 
 # Function for updating nginx config
@@ -46,7 +59,7 @@ function update_nginx_config() {
     # Backup the current configuration file if backup doesn't exist
     if [ ! -f "${config_file}.backup" ]; then
         sudo cp "$config_file" "${config_file}.backup"
-        # echo -e "    Backed up the current configuration file to ${brown}${config_file}.backup${reset}"
+        echo -e "    Backed up the current configuration file to ${brown}${config_file}.backup${reset}"
     fi
 
     # Remove any existing configuration for serving hbnb_static
@@ -87,7 +100,7 @@ directories=(
     "/data/web_static/releases/test/"
 )
 
-# echo -e "${blue}Setting up your web server & doing some minor checks...${reset}"
+echo -e "${blue}Setting up your web server & doing some minor checks...${reset}"
 
 # Install packages
 for package in "${packages[@]}"; do
@@ -109,7 +122,10 @@ if [ -L "$symbolic_link" ]; then
 fi
 ln -s "/data/web_static/releases/test/" "$symbolic_link"
 
-# echo -e "${blue}Updating Nginx configuration.${reset}"
+# Check and update ownership of /data/ to ``ubuntu``
+check_owner
+
+echo -e "${blue}Updating Nginx configuration.${reset}"
 
 # Update Nginx configuration
 update_nginx_config || handle_error 1 "Failed to update Nginx configuration"
@@ -117,6 +133,6 @@ update_nginx_config || handle_error 1 "Failed to update Nginx configuration"
 # Restart Nginx
 restart_nginx || handle_error 1 "Failed to restart Nginx"
 
-# echo -e "${blue}Setting up Done!${reset}"
+echo -e "${blue}Setting up Done!${reset}"
 # Successful exit
 exit 0
