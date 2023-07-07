30479aa1cde85f8398cb42455b659a0eba7f632a
FourtyThree43 <shaqmwa@outlook.com>
2023-07-07 11:50:25 +0300

    Rewrite

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,28 +7,20 @@ from fabric.api import local
 
 def do_pack():
     """Create a .tgz archive from the contents of the web_static folder."""
-    try:
-        if not os.path.exists("versions"):
-            print("Creating versions directory")
-            os.mkdir("versions")
+    if not os.path.exists("versions"):
+        print("Creating versions directory")
+        os.mkdir("versions")
 
-        now = datetime.now()
-        archive_name = "web_static_{}{}{}{}{}{}.tgz".format(
-            now.year, now.month, now.day, now.hour, now.minute, now.second
-        )
-        archive_path = "versions/{}".format(archive_name)
-        archive_command = "tar -czvf {} web_static".format(archive_path)
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_name = "web_static_{}.tgz".format(now)
+    archive_path = "versions/{}".format(archive_name)
 
-        print("Packing web_static to: {}".format(archive_path))
-        local(archive_command)
+    print("Packing web_static to: {}".format(archive_path))
+    archive_command = local("tar -czvf {} web_static".format(archive_path))
 
-        if os.path.exists(archive_path):
-            print("Archive created successfully: {}".format(archive_path))
-            return archive_path
-        else:
-            print("Failed to create archive.")
-            return None
-
-    except Exception as e:
-        print("Error occurred during archive creation:", str(e))
+    if archive_command.succeeded:
+        print("Archive created successfully: {}".format(archive_path))
+        return archive_path
+    else:
+        print("Failed to create archive.")
         return None
