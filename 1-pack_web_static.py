f4d910bd8e9fce982689f185e2c89b76fbde1e09
FourtyThree43 <shaqmwa@outlook.com>
2023-07-09 23:51:28 +0300

    Fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,18 +7,19 @@ from fabric.api import local
 
 def do_pack():
     """Create a .tgz archive from the contents of the web_static folder."""
-    if not os.path.exists("versions"):
-        os.mkdir("versions")
+
+    local("mkdir -p versions")
 
     now = datetime.now().strftime("%Y%m%d%H%M%S")
     archive_name = "web_static_{}.tgz".format(now)
     archive_path = "versions/{}".format(archive_name)
 
-    try:
-        print("Packing web_static to {}".format(archive_path))
-        local("tar -cvzf {} web_static".format(archive_path))
-        size = os.stat(archive_path).st_size
-        print("web_static packed: {} -> {} Bytes".format(archive_path, size))
-    except Exception:
-        archive_path = None
-    return archive_path
+    print("Packing web_static to {}".format(archive_path))
+    archive = local("tar -cvzf {} web_static".format(archive_path))
+    size = os.stat(archive_path).st_size
+    print("web_static packed: {} -> {}Bytes".format(archive_path, size))
+
+    if archive.succeeded:
+        return archive_path
+    else:
+        return None
