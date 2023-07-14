6ab48796ce441250c20793f75ef3aa43fcc334be
FourtyThree43 <shaqmwa@outlook.com>
2023-07-14 11:58:21 +0300

    print staments - Timestamp: 1689325101

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,12 +11,15 @@ env.user = "ubuntu"
 def do_pack():
     """Create a .tgz archive from the contents of the web_static folder."""
     try:
-        local("mkdir -p versions")
+        if not os.path.isdir("versions"):
+            os.mkdir("versions")
         now = datetime.now().strftime("%Y%m%d%H%M%S")
         archive_name = "web_static_{}.tgz".format(now)
         archive_path = "versions/{}".format(archive_name)
+        print("Packing web_static to {}".format(archive_path))
         local("tar -cvzf {} web_static".format(archive_path))
-        print("Archive created:", archive_path)
+        size = os.stat(archive_path).st_size
+        print("web_static packed: {} -> {}Bytes".format(archive_path, size))
         return archive_path
     except Exception as e:
         print("Archive creation failed:", str(e))
@@ -34,7 +37,7 @@ def do_deploy(archive_path):
         filename = os.path.basename(archive_path)
         folder_name = "/data/web_static/releases/{}".format(
                                                       filename.split(".")[0]
-                                                      )
+                                                    )
 
         # Uncompress the archive to the folder
         run("mkdir -p {}".format(folder_name))
